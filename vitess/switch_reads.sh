30518191ff4c86b3a2b2519ea252acb872ce1eec
Andrey Egoshin <andrey.egoshin@ispringsolutions.com>
2022-04-24 08:51:11 +0300

    Реализовал шардирование с использованием vitess

--- vitess/switch_reads.sh
+++ vitess/switch_reads.sh
@@ -6,4 +6,5 @@ if [[ "$OSTYPE" == "msys" ]]; then
         tty=winpty
 fi
 
-exec $tty docker-compose exec ${CS:-vtctld} /vt/bin/vtctlclient -server vtctld:15999 Reshard --source_shards '0' --target_shards '-80,80-' Create conversation_keyspace.reshard
+exec $tty docker-compose exec ${CS:-vtctld} /vt/bin/vtctlclient -server vtctld:15999 Reshard -- --tablet_types=rdonly,replica SwitchTraffic conversation_keyspace.reshard
+
